9f52fcc882836c67a0d16abf54dc6b690879da95
Cairo R. Oliveira <hello@cairoramos.com>
2016-11-03 01:57:37 -0200

    Bugfix and improvements on gulpfile and other files.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,39 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 
 var angularPath = './node_modules/angular/angular.min.js',
+    materiallizeJs = [
+        './node_modules/materialize-css/js/initial.js',
+        './node_modules/materialize-css/js/jquery.easing.1.3.js',
+        './node_modules/materialize-css/js/animation.js',
+        './node_modules/materialize-css/js/velocity.min.js',
+        './node_modules/materialize-css/js/hammer.min.js',
+        './node_modules/materialize-css/js/jquery.hammer.js',
+        './node_modules/materialize-css/js/global.js',
+        './node_modules/materialize-css/js/collapsible.js',
+        './node_modules/materialize-css/js/dropdown.js',
+        './node_modules/materialize-css/js/modal.js',
+        './node_modules/materialize-css/js/materialbox.js',
+        './node_modules/materialize-css/js/parallax.js',
+        './node_modules/materialize-css/js/tabs.js',
+        './node_modules/materialize-css/js/tooltip.js',
+        './node_modules/materialize-css/js/waves.js',
+        './node_modules/materialize-css/js/toasts.js',
+        './node_modules/materialize-css/js/sideNav.js',
+        './node_modules/materialize-css/js/scrollspy.js',
+        './node_modules/materialize-css/js/forms.js',
+        './node_modules/materialize-css/js/slider.js',
+        './node_modules/materialize-css/js/cards.js',
+        './node_modules/materialize-css/js/chips.js',
+        './node_modules/materialize-css/js/pushpin.js',
+        './node_modules/materialize-css/js/buttons.js',
+        './node_modules/materialize-css/js/transitions.js',
+        './node_modules/materialize-css/js/scrollFire.js',
+        './node_modules/materialize-css/js/date_picker/picker.js',
+        './node_modules/materialize-css/js/date_picker/picker.date.js',
+        './node_modules/materialize-css/js/character_counter.js',
+        './node_modules/materialize-css/js/carousel.js'
+    ],
+    jqueryPath = './node_modules/jquery/dist/jquery.min.js',
     jsDest = './public_html/libs/js',
     materiallizeSass = './node_modules/materialize-css/sass/materialize.scss',
     cssDest = './public_html/libs/css';
@@ -17,15 +50,28 @@ gulp.task('angularJs', function(){
         .pipe(gulp.dest(jsDest));
 });
 
-gulp.task('materiallize', function(){
+gulp.task('materialSass', function(){
     return gulp.src(materiallizeSass)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(cssDest));
 });
 
+gulp.task('materialJs', function(){
+    return gulp.src(materiallizeJs)
+        .pipe(concat('materiallize.js'))
+        .pipe(gulp.dest(jsDest));
+});
+
+gulp.task('copyScript', function(){
+    return gulp.src(jqueryPath)
+        .pipe(gulp.dest(jsDest));
+});
+
 gulp.task('default', function(){
     gulp.run([
         'angularJs',
-        'materiallize'
+        'materialSass',
+        'copyScript',
+        'materialJs'
     ]);
 });
---
+++ public_html/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Products</title>
+
+    <!-- Material Design Style Sheets -->
+    <link rel="stylesheet" href="libs/css/materialize.css">
+
+    <!-- Material Design Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+
+<!-- Jquery -->
+<script src=""></script>
+</body>
+</html>
